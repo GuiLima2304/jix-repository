165b7a948d9bae83a2f0f6bf2768046d7c3fa812
Guilherme <gui.lima2304@gmail.com>
2024-04-28 19:26:21 -0300

    initial commit

---
+++ src/main/java/com/project/jix/JixProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.jix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JixProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JixProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/jix/JixProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JixProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
